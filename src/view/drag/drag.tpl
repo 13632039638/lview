610629c24a38960f4c2a5033cf93c62f25ef0c90
975949531@qq.com <975949531@qq.com>
2021-03-07 20:25:37 +0800

    feat:完成置顶排序

--- src/view/drag/drag.tpl
+++ src/view/drag/drag.tpl
@@ -1,9 +1,9 @@
 <div class="drag " id="drag_{{ id }}">
     <div id="hg_{{index}}" class="default bg-white"></div>
-    <div id="{{ index }}"  index="{{ index }}" class="d-flex px-4 jc-between drag-main ai-center bg-white">
+    <div id="{{ index }}"  index="{{ index }}" class="d-flex px-4 jc-between drag-main ai-center bg-white lg_{{index}}">
         <span>{{ title }}</span>
         <div class="w5 d-flex ai-center jc-center">
-            <img src="https://ftp.bmp.ovh/imgs/2021/03/b0b5f154b0509a6c.png" >
+            <img id="{{ index }}" src="https://ftp.bmp.ovh/imgs/2021/03/b0b5f154b0509a6c.png" >
         </div>
     </div>
 </div>
--- src/view/drag/index.js
+++ src/view/drag/index.js
@@ -1,5 +1,6 @@
 import tpl from './drag.tpl'
 import './drag.scss'
+import $ from 'jquery'
 
 import { tplReplace } from '../../utils/tools'
 
@@ -24,6 +25,7 @@ export default _ => {
     ];
     let dragIndex = -1,
         idx = -1,
+        eletop = 0,
         isflag = false;
     return {
         name: 'drag',
@@ -44,7 +46,7 @@ export default _ => {
             const drag = document.querySelector('.container ');
             drag.addEventListener( 'touchmove', this.touchMove.bind(this), false );
             drag.addEventListener( 'touchend', this.touchEnd.bind(this, fn ), false );
-            // drag.addEventListener( 'click', this.Change.bind(this), false );
+            drag.addEventListener( 'click', this.Change.bind(this, fn), false );
         },
 
         touchMove( ev ) {
@@ -54,10 +56,12 @@ export default _ => {
                 Index = Math.floor( (ev.changedTouches[0].clientY - tophg) / ev.target.offsetHeight ),          // 滑动的距离 - 顶部高度 / 盒子高度 = 当前下标
                 Afterhg = document.getElementById(`hg_${Index}`);                       // 当前鼠标经过的盒子
 
+            // 如果触发了滑动 则能触发结束事件
+            isflag = true;
             // 存储拖拽的盒子下标
             idx = tag.id;      
             // 存储鼠标经过的盒子的下标
-            dragIndex = Index;
+            dragIndex = Index < 0 ? 0 : Index;
             // 先吧折叠的盒子高度初始化
             this.defaluthg();
 
@@ -73,11 +77,16 @@ export default _ => {
         },
 
         touchEnd( ev ) {
+            // isflag 为假的话 直接退出
+            if ( !isflag ) return;
+            console.log( '去时候进行', dragIndex );
+
             let data = _obj[idx];
             _obj.splice( idx, 1 );
             _obj.splice( dragIndex, 0, data );
             // 执行父组件的事件  重新渲染列表
             arguments[0]();
+            isflag = false;
         },
 
         defaluthg() {
@@ -88,7 +97,28 @@ export default _ => {
         },
 
         Change() {
-            console.log( '冲突' );
+
+            arguments[1].preventDefault();
+            const Index = arguments[1].target.id,
+                    elehg = $(`.lg_${Index}`).height();
+
+            if ( Index == '' ) return;
+
+            for( let i = 0; i < Index; i++) {
+                eletop += elehg;
+                $(`.lg_${i}`).css({ "transition": "all .3s", "transform": `translateY(${elehg}px)` });
+            }
+
+            $(`.lg_${Index}`).css({ "transition": 'all .3s', "transform": `translateY(-${eletop}px)` });
+
+            let data = _obj[Index];
+            setTimeout( _ => {
+                _obj.splice( Index, 1 );
+                _obj.unshift( data );
+                eletop = 0;
+                arguments[0]();
+            },500);
+
         }
 
     }
